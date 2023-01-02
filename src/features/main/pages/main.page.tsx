80c905876f3cfb8d38c52f20859059e314e0543f
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-02 18:23:00 +0100

    :rotating_light: fix linter warnings

--- src/features/main/pages/main.page.tsx
+++ src/features/main/pages/main.page.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react';
 export function MainPage() {
     const repo = new RobotRepository();
     const initialRobots = Array<RobotInfo>;
-   
+
     const [robots, setRobots] = useState<RobotInfo[]>(initialRobots);
     const handleDelete = async (id: string) => {
         console.log(id);
@@ -18,14 +18,13 @@ export function MainPage() {
         await repo.create(robot);
         setRobots([...robots, robot]);
     };
-     useEffect(() => {
-         repo.load().then((robots) => setRobots(robots));
-         
-     });
+    useEffect(() => {
+        repo.load().then((robots) => setRobots(robots));
+    });
     return (
         <div>
             <h1>Main page</h1>
-            <RobotAdd handleAdd={handleAdd} ></RobotAdd>
+            <RobotAdd handleAdd={handleAdd}></RobotAdd>
             <RobotList handleDelete={handleDelete} robots={robots}></RobotList>
         </div>
     );
