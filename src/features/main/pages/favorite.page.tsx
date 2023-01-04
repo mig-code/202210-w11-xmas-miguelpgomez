d49b65a064407b1cfe7845c16619edcda49c70c1
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-04 12:58:05 +0100

    :construction: Update with useRobot Hook

--- src/features/main/pages/favorite.page.tsx
+++ src/features/main/pages/favorite.page.tsx
@@ -1,37 +1,11 @@
-import { RobotRepository } from '../../../core/services/repository';
-import { RobotInfo } from '../../../core/types/robots.type';
-
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useEffect } from 'react';
 import { FavoriteList } from '../components/favorite.list/favorite.list';
+import { useRobots } from '../../../core/components/hooks/useRobots';
 
 export function FavoritePage() {
-    const repo = useMemo(() => new RobotRepository(), []);
-    const initialRobots = Array<RobotInfo>;
-
-    const [robots, setRobots] = useState<RobotInfo[]>(initialRobots);
-    const handleDelete = async (id: string) => {
-        console.log(id);
-        await repo.delete(id);
-        setRobots(robots.filter((robot) => robot.id !== id));
-    };
-
-    const handleFavorite = async (robot: Partial<RobotInfo>) => {
-        console.log(robot);
-        robot.isFavorite = !robot.isFavorite;
-        await repo.update(robot);
-        setRobots(
-            robots.map((item) =>
-                item.id === robot.id ? { ...item, ...robot } : item
-            )
-        );
-    };
-    const handleLoad = useCallback(async () => {
-        const robots = await repo.load();
-        setRobots(robots);
-    }, [repo]);
+    const { robots, handleDelete, handleFavorite, handleLoad } = useRobots();
     useEffect(() => {
         handleLoad();
-        console.log('useEffect');
     }, [handleLoad]);
     return (
         <div>
--- src/features/main/pages/main.page.tsx
+++ src/features/main/pages/main.page.tsx
@@ -1,40 +1,14 @@
-import { RobotRepository } from '../../../core/services/repository';
-import { RobotInfo } from '../../../core/types/robots.type';
 import { RobotAdd } from '../components/robot.add/robot.add';
 import { RobotList } from '../components/robot.list/robot.list';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useEffect } from 'react';
+import { useRobots } from '../../../core/components/hooks/useRobots';
 
 export function MainPage() {
-    const repo = useMemo(() => new RobotRepository(), []);
-    const initialRobots = Array<RobotInfo>;
-
-    const [robots, setRobots] = useState<RobotInfo[]>(initialRobots);
-    const handleDelete = async (id: string) => {
-        console.log(id);
-        await repo.delete(id);
-        setRobots(robots.filter((robot) => robot.id !== id));
-    };
-    const handleAdd = async (robot: RobotInfo) => {
-        await repo.create(robot);
-        setRobots([...robots, robot]);
-    };
-    const handleFavorite = async (robot: Partial<RobotInfo>) => {
-        console.log(robot);
-        robot.isFavorite = !robot.isFavorite;
-        await repo.update(robot);
-        setRobots(
-            robots.map((item) =>
-                item.id === robot.id ? { ...item, ...robot } : item
-            )
-        );
-    };
-    const handleLoad = useCallback(async () => {
-        const robots = await repo.load();
-        setRobots(robots);
-    }, [repo]);
+    const { robots, handleDelete, handleAdd, handleFavorite, handleLoad } =
+        useRobots();
     useEffect(() => {
         handleLoad();
-        console.log('useEffect');
+        
     }, [handleLoad]);
     return (
         <div>
