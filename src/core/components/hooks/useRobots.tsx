76e43867f1cff39ed7b963a3fd6d84aa0f3d6990
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-04 12:57:14 +0100

    :construction: Add useRobot hook

---
+++ src/core/components/hooks/useRobots.tsx
@@ -0,0 +1,45 @@
+import { useCallback, useMemo, useState } from 'react';
+import { RobotRepository } from '../../services/repository';
+import { setStorageList } from '../../services/storage';
+import { RobotInfo } from '../../types/robots.type';
+
+export function useRobots() {
+    const repo = useMemo(() => new RobotRepository(), []);
+    const initialRobots = Array<RobotInfo>;
+
+    const [robots, setRobots] = useState<RobotInfo[]>(initialRobots);
+    const handleDelete = async (id: string) => {
+        console.log(id);
+        await repo.delete(id);
+        setRobots(
+            robots.filter((robot: Partial<RobotInfo>) => robot.id !== id)
+        );
+        setStorageList('robots', [robots.length - 1]);
+    };
+    const handleAdd = async (robot: RobotInfo) => {
+        await repo.create(robot);
+        setRobots([...robots, robot]);
+        setStorageList("robots", [robots.length +1])
+    };
+    const handleFavorite = async (robot: Partial<RobotInfo>) => {
+        console.log(robot);
+        robot.isFavorite = !robot.isFavorite;
+        await repo.update(robot);
+        setRobots(
+            robots.map((item) =>
+                item.id === robot.id ? { ...item, ...robot } : item
+            )
+        );
+    };
+    const handleLoad = useCallback(async () => {
+        const robots = await repo.load();
+        setRobots(robots);
+    }, [repo]);
+    return {
+        robots,
+        handleDelete,
+        handleAdd,
+        handleFavorite,
+        handleLoad,
+    };
+}
