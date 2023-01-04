36f49303929ceee1e92e0ae7cd0201cecafcf6e8
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-04 19:11:46 +0100

    :white_check_mark: add favorite.list test

---
+++ src/features/main/mocks/mock.robot.ts
@@ -0,0 +1,21 @@
+import { RobotInfo } from "../models/robots.models";
+
+export const arrayRobotsMock: Array<RobotInfo> = [{
+            "name": "test1",
+            "speed": 1,
+            "resistance": 0,
+            "user": " user 1",
+            "id": "123860",
+            "date": "2/1/2023",
+            "isFavorite": true
+        },
+        {
+            "name": "test 2",
+            "speed": 8,
+            "resistance": 4,
+            "user": "user 2",
+            "id": "769364",
+            "date": "4/1/2023",
+            "isFavorite": false
+        },
+]
