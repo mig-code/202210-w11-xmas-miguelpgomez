e48e4b1733932a5d9cfebf703d1382efb21f9dcf
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-04 18:53:07 +0100

    :white_check_mark: Add service tests

--- src/core/services/repository/repository.ts
+++ src/core/services/repository/repository.ts
@@ -1,5 +1,5 @@
-import { Repository } from '../interfaces/repository.interface';
-import { RobotInfo } from '../../features/main/models/robots.models';
+import { Repository } from '../../interfaces/repository.interface';
+import { RobotInfo } from '../../../features/main/models/robots.models';
 
 const invalidIdError = new Error('Invalid ID');
 
--- src/core/services/storage.ts
+++
@@ -1,9 +0,0 @@
-export const getStorageList = <T>(storeName: string): Array<T> => {
-    const result = localStorage.getItem(storeName);
-    if (!result) return [];
-    return JSON.parse(result);
-};
-
-export const setStorageList = <T>(storeName: string, data: Array<T>): void => {
-    localStorage.setItem(storeName, JSON.stringify(data));
-};
